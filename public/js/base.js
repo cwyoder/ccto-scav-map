e91827d2cf085d3e762d38c33f323154b9ae63f5
Chad Yoder <cwyoder13@gmail.com>
2022-01-10 15:10:45 -0600

    preparing to ship code to github

--- public/js/base.js
+++ public/js/base.js
@@ -1,7 +1,7 @@
 // TO MAKE THE MAP APPEAR YOU MUST
 // ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
-mapboxgl.accessToken = 'pk.eyJ1IjoibHVjaWRhLW1hcHMiLCJhIjoiY2t5MjM3NGZmMGd3bzJxbWZ0a3p3c2c1OSJ9.l0b6hje-1TFmM8JQFDuD4A';
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVwb2Rtb2xpayIsImEiOiJja3k5NmE4NmIwMWgzMm9tZ3RteTYyaXY3In0.VFTaG_ZP3juiVbJRDBmSjw';
 const boundaries = [
   [-89.340000, 40.892000], // Southwest
   [-86.118000, 42.579000] // Northeast
